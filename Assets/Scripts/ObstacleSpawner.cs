b93b863777899cd5a25eca1266dff07b8b3bab31
CheongYanSeng <64124487+CheongYanSeng@users.noreply.github.com>
2020-06-22 12:18:01 +0800

    Edited
    Project

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public float SpawnTime;
+    public GameObject[] obstacles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnTime += Time.deltaTime;
+        if(SpawnTime > 1.5f)
+        {
+            float xPos = Random.Range(-1.5f, 1.5f);
+            int spawned = Random.Range(0, 4);
+            Instantiate(obstacles[spawned], new Vector3(xPos, 0, 0), Quaternion.identity);
+            SpawnTime = 0;
+        }
+    }
+}
